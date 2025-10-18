bdd028ccb204da1323207e320c329ebac8a80525
Mohit Kansana <mohitkansana82@gmail.com>
2025-10-18 06:11:14 +0000

    ओ भाई लाइव डाउट सॉल्वर है उसे अक क्यों मनाया उसमें खुद कोई इंसान सॉल्व क

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,7 +1,7 @@
+'use server';
 import { config } from 'dotenv';
 config();
 
 import '@/ai/flows/generate-practice-test.ts';
 import '@/ai/flows/get-ai-tutoring.ts';
 import '@/ai/flows/vidya-search.ts';
-import '@/ai/flows/live-doubt-flow.ts';
